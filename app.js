fc59c35967f3db8a1cb086a2defc5171b9f0a990
Sanket Patel <sanketpatel.301090@gmail.com>
2018-04-26 14:49:50 -0500

    Add db config

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 require('log-timestamp');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongo = require('mongodb');
+const mongoClient = mongo.MongoClient;
+
 const app = express();
 app.use(bodyParser.json());
-
 console.log('App started!');
 
-let allUsers = [];
+let mongoHost = '35.174.100.96';
+let mongoPort = 27017;
+let dbName = 'mydb';
+
+let url = `mongodb://${mongoHost}:${mongoPort}/${dbName}`;
+mongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    console.log("Database connected");
+    db.close();
+});
+
 function getRootResponse(req) {
     return ' <h1>Hello World!</h1><p>This is a simple response to a <b>GET</b> request on the base path</p>\n';
 }
@@ -20,13 +32,14 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     console.log(`Received a GET request on ${req.path} from ${req.hostname}`);
+    let allUsers = [];
     res.send(allUsers);
     console.log(`Response sent for GET ${req.path} to ${req.hostname}`);
 });
 
 app.post('/users', (req, res) => {
     console.log(`Received a POST request on ${req.path} from ${req.hostname}`);
-    allUsers.push(req.body);
+    
     res.send('Success!');
     console.log(`Response sent for POST ${req.path} to ${req.hostname}`);
 });
