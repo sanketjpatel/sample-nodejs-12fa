fd2f27e2321cb48f23345d9e616b0bfe2642b839
Sanket Patel <sanketpatel.301090@gmail.com>
2018-04-26 13:34:32 -0500

    Add log-timestamp and get, post /users path

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+require('log-timestamp');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
-function getRootResponse(){
-    return '<h1>Hello World!</h1>'
+console.log('App started!');
+
+let allUsers = [];
+function getRootResponse(req){
+    return ' <h1>Hello World!</h1><p>This is a simple response to a <b>GET</b> request on the base path</p>';
 }
 
-app.get('/', (req, res) => res.send(getRootResponse()));
+console.log('Registering routes');
+app.get('/', (req, res) => {
+    console.log(`Received a GET request on ${req.path} from ${req.host}`);
+    res.send(getRootResponse(req));
+    console.log(`Response sent for GET ${req.path} to ${req.host}`);
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/users', (req, res) => {
+    console.log(`Received a GET request on ${req.path} from ${req.host}`);
+    res.send(allUsers);
+    console.log(`Response sent for GET ${req.path} to ${req.host}`);
+});
+
+app.post('/users', (req, res) => {
+    console.log(`Received a POST request on ${req.path} from ${req.host}`);
+    allUsers.push(req.body);
+    res.send('Success!');
+    console.log(`Response sent for POST ${req.path} to ${req.host}`);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
